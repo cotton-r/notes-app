d6670feb6708790c356cab78ec43e0b9af500af3
Rory Cotton <66560099+cotton-r@users.noreply.github.com>
2022-09-12 00:51:43 +0100

    render note content based on state

--- src/components/Note.js
+++ src/components/Note.js
@@ -1,12 +1,12 @@
 import React from 'react';
 import { MdDeleteForever } from 'react-icons/md';
 
-const Note = () => {
+const Note = ({ id, text, date }) => {
   return (
     <div className='note'>
-        <span>Hello! This is my first note.</span>
+        <span>{text}</span>
         <div className='note-footer'>
-            <small>12/09/2022</small>
+            <small>{date}</small>
             <MdDeleteForever className='delete-icon' size='1.3em' />
         </div>
     </div>
