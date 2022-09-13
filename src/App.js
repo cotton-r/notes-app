bbcaa536adc987dfac3ceb2083e6e8c663ba7ace
Rory Cotton <66560099+cotton-r@users.noreply.github.com>
2022-09-14 00:49:54 +0100

    allow notes to be deleted

--- src/App.js
+++ src/App.js
@@ -37,10 +37,19 @@ const App = () => {
     setNotes(newNotes);
   };
 
+  const deleteNote = (id) => {
+    const newNotes = notes.filter((note) => note.id !== id);
+    setNotes(newNotes);
+  }
+
 
   return (
     <div className="container">
-      <NotesList notes={notes} handleAddNote={addNote} />
+      <NotesList 
+        notes={notes} 
+        handleAddNote={addNote} 
+        handleDeleteNote={deleteNote}
+      />
     </div>
   )
 };
